3f5ce6af9a2f389f2d3b026b7b1dccb40b79b411
Bernd-L <git@bernd.pw>
2020-02-07 13:32:39 +0100

    progress

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { readFileSync } from "fs";
 import { join } from "path";
 import { Auth } from "./models/auth";
 import { parse } from "date-fns";
+import { de } from "date-fns/locale";
 
 class Main {
   /**
@@ -32,25 +33,41 @@ class Main {
    */
   async main() {
     // Perform the authentication
-    await this.untis.login();
+    // await this.untis.login();
 
     /**
      * My own timetable
      */
-    const timetable = await this.untis.getOwnTimetableForToday();
+    // const timetable = await this.untis.getOwnTimetableForToday();
+    const timetable = JSON.parse(
+      readFileSync(join(__dirname, "../private/Untitled-1.json")).toString()
+    );
 
     // TODO Remove this log
     log(JSON.stringify(timetable));
 
     timetable.map((l: Lesson, i: number) => {
-      const;
-      const na = parse(l.startTime + "", "yyyyMMdd", new Date());
+      const timeWithPadding = this.addPaddingToNumber(l.startTime);
+      log("Time with padding: " + timeWithPadding);
+      const na = new Date()(
+        timeWithPadding,
+        "hhmm",
+        new Date().getTimezoneOffset()
+      );
+
+      log(na);
     });
   }
 
-  private dateTransform = (untisDate: number) => {};
+  private dateTransform = (untisDate: number): Date =>
+    new Date(
+      new Date().getFullYear,
+      new Date().getMonth(),
+      new Date().getDate,
+      untisDate
+    );
   private addPaddingToNumber = (untisTime: number): string =>
-    untisTime >= 1000 ? "0" + untisTime : untisTime;
+    untisTime < 1000 ? "0" + untisTime : untisTime + "";
 }
 
 // Start the program
